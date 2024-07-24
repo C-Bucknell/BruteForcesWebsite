59c41cb84e67715760e9ee83f5a9b42c9094a418
Chris Bucknell <99325130+C-Bucknell@users.noreply.github.com>
2024-07-24 21:09:43 +1000

    Create aminpage.php
    Create aminpage.php

---
+++ aminpage.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Logged in</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href=
+"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+  </head>
+  <body>
+<div class="login-box">
+    <h2> Hello - you've hacked my site!!! </h2>
+</div>
+  </body>
+</html>
